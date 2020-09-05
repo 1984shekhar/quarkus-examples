915634ec069dc6314a8eb39628eabcbbc3d938dd
Chandra Shekhar <chandrashekhar@localhost.localdomain>
2020-09-05 14:30:09 +0530

    quarkus camel xml example

---
+++ quarkus-camel-xml/src/test/java/org/quarkus/timer/xml/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.quarkus.timer.xml;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
